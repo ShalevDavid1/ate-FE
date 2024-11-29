28b153c5e0f432e51cf8a6dd4a76cf2de831585d
ShalevDavid1 <shalevd2014@gmail.com>
2024-11-29 18:47:14 +0200

    added about option to side bar and basic about me page

--- src/components/bar/SideBar.tsx
+++ src/components/bar/SideBar.tsx
@@ -1,7 +1,16 @@
 import "./Sidebar.css";
 
+import { useNavigate } from "react-router-dom";
+
 
 const Sidebar = () => {
+
+    const navigate = useNavigate();
+
+    const handleAboutClick = () => {
+        navigate("/about")
+    }
+
     const handleOptionClick = (option: string) => {
         console.log(`${option} clicked`);
         // Add navigation logic here if needed
@@ -12,7 +21,7 @@ const Sidebar = () => {
             <div className="sidebar-title">ATE</div>
             <ul className="sidebar-options">
                 <li onClick={() => handleOptionClick("Friends")}>Friends</li>
-                <li onClick={() => handleOptionClick("About")}>About</li>
+                <li onClick={handleAboutClick}>About</li>
             </ul>
         </div>
     );
